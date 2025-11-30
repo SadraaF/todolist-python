12a72c61d91282ccbbf61f14c5a01add7b21e6c7
Sadra1122 <Sadrafadaei@gmail.com>
2025-11-30 21:03:14 +0330

    feat: implement list and create endpoints for tasks

--- src/app/api/controllers/projects_controller.py
+++ src/app/api/controllers/projects_controller.py
@@ -13,20 +13,12 @@ from src.app.core.config import get_settings
 from src.app.db.session import get_db
 from src.app.repositories.sqlalchemy_repository import SqlAlchemyProjectRepository
 from src.app.services.project_service import ProjectService
+from src.app.api.dependencies import get_project_service
 from sqlalchemy.orm import Session
 
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
 
-def get_project_service(db: Session = Depends(get_db)) -> ProjectService:
-    """Dependency injector for the ProjectService."""
-    settings = get_settings()
-    repo = SqlAlchemyProjectRepository(session=db)
-    return ProjectService(
-        repo=repo,
-        max_projects=settings.MAX_NUMBER_OF_PROJECT,
-        max_tasks=settings.MAX_NUMBER_OF_TASK,
-    )
 
 @router.get("", response_model=list[ProjectResponse])
 def list_projects(
